06e942d1104c67d8c6f7717629f56aa18def07c1
Viney <vineykhaneja999@gmail.com>
2022-04-02 22:34:06 +0530

    [Viney] ADDED: Ability to add Employee Object Through REST

--- EmployeePayrollTest/PayrollUnitTests.cs
+++ EmployeePayrollTest/PayrollUnitTests.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using System;
+using Newtonsoft.Json.Linq;
 
 namespace EmployeePayrollTest
 {
@@ -16,22 +17,40 @@ namespace EmployeePayrollTest
         private IRestResponse GetEmployeeList()
         {
             //Arrange
-            RestRequest request = new RestRequest("/posts",Method.GET);
+            RestRequest request = new RestRequest("/posts", Method.GET);
             //Act
             IRestResponse response = restClient.Execute(request);
             return response;
         }
         [TestMethod]
-        public void GivenCorrectLocalHost_InRestRequest_ShouldReturnListofEmployeesAPI()
+        public void GivenCorrectLocalHostAndGETMethod_InRestRequest_ShouldReturnListofEmployeesAPI()
         {
             IRestResponse response = GetEmployeeList();
-            Assert.AreEqual(response.StatusCode,HttpStatusCode.OK);
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
             List<Employee> dataResponse = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
-            Assert.AreEqual(7,dataResponse.Count);
-            foreach(Employee emp in dataResponse)
+            Assert.AreEqual(8, dataResponse.Count);
+            foreach (Employee emp in dataResponse)
             {
                 Console.WriteLine(emp.ToString());
             }
+        }
+        [TestMethod]
+        public void GivenCorrectLocalHostAndPOSTMethod_InRestRequest_ShouldReturnListofEmployeesAddedRecently()
+        {
+            //Arrange
+            RestRequest restRequest = new RestRequest("/posts",Method.POST);
+            JObject jobjBody = new JObject();
+            jobjBody.Add("name", "bhushan");
+            jobjBody.Add("salary", "92000");
+            restRequest.AddParameter("application/json",jobjBody,ParameterType.RequestBody);
+            //Act
+            IRestResponse restResponse = restClient.Execute(restRequest);
+            //Assert
+            Assert.AreEqual(restResponse.StatusCode, HttpStatusCode.Created);
+            Employee employee = JsonConvert.DeserializeObject<Employee>(restResponse.Content);
+            Assert.AreEqual("bhushan",employee.name);
+            Assert.AreEqual("92000",employee.salary);
+            Console.WriteLine(restResponse.Content);
         }
     }
 }
